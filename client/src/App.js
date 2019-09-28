dd22bb4457d46655257a57ce842e005dbe27326a
Bosco <nzibosco@gmail.com>
2019-09-27 21:13:43 -0500

    home and members pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Menu from "./pages/Menu";
-// import Nav from "./components/nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Members from "./pages/Members";
+import Nav from "./components/nav";
 
 
 function App() {
 return (
-//     <Router>
-//       <div>
-//         <Nav />
-//         <Switch>
-//           <Route exact path="/" component={Home} />
-//           <Route exact path="/menu" component={Menu} />
-//           {/* <Route exact path="/books/:id" component={Detail} /> */}
-//         </Switch>
-//       </div>
-//     </Router>
-<div>
-    <h1 className = "display">Hello Everyone</h1>
-</div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/members" component={Members} />
+          {/* <Route exact path="/books/:id" component={Detail} /> */}
+        </Switch>
+      </div>
+    </Router>
    );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,28 @@
-// import React, { Component } from "react";
-// import Jumbotron from "../components/Jumbotron";
-// import { Col, Row, Container } from "../components/Grid";
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import { Col, Row, Container } from "../components/Grid";
 
-// function Home() {
-//     return (
-//         <Container fluid>
-//             <Jumbotron fluid>
-//                 <h1 className="Display-4">
-//                     Menu Ville
-//                </h1>
-//                <p className = "lead">
-//                     Get inside our delicious menu and seamlessly order your preferred dishes
-//               </p>
-//             </Jumbotron>
-//             <Row>
-//                 <Col size = "md-12">
-//                     <Jumbotron>
-//                        <a className = "display-4" href = "/menu">Go to the menu now</a>
-//                     </Jumbotron>
-//                 </Col>
-//             </Row>
+function Home() {
+    return (
+        <Container fluid>
+            <Jumbotron fluid>
+                <h1 className="Display-4">
+                    MT HOUSE
+               </h1>
+               <p className = "lead">
+                    Welcome to Mutual Trust House. A dynamic group of young people committed to making a difference through savings!
+              </p>
+            </Jumbotron>
+            <Row>
+                <Col size = "md-12">
+                    <Jumbotron>
+                       <a className = "display-4" href = "/members">Go to dashboard</a>
+                    </Jumbotron>
+                </Col>
+            </Row>
 
-//         </Container>
-//     );
-// }
+        </Container>
+    );
+}
 
-// export default Home;
+export default Home;
