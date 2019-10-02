9342cc20584ff01a9d13fd386706ed68de7f157f
Bosco <nzibosco@gmail.com>
2019-10-02 15:15:29 -0500

    db model for contributions

---
+++ models/contribution.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize, DataTypes) {
+    // model to record member's contributions
+    const Contribution = sequelize.define("Contribution", {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      member: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 100]
+        }
+      },
+      contribution: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      month: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      year: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      bankSlip: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
+
+    // foreign key to link contributions to their members. 
+    // contributions cannot be created without members.
+
+    Contribution.associate = function(models) {
+        Contribution.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+  
+    return Contribution;
+  };
