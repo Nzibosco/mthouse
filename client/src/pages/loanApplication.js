7f1ca3e1a189c111725d609256d60cccf4cadbc8
Bosco <nzibosco@gmail.com>
2019-10-04 20:12:39 -0500

    loan application page

---
+++ client/src/pages/loanApplication.js
@@ -0,0 +1,4 @@
+// this page is for users to apply for loans
+// members send request for the loan amount they want
+// members propose the loan repayment period 
+// an API request is sent to the admin or finance team to approve the request and disburse the loan.
