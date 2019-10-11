38dcb82997c8b2cbd53170dd262b8eafb6a0b58c
Bosco <nzibosco@gmail.com>
2019-10-11 14:28:13 -0500

    added dashboard

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,8 @@
+// To highlight measure info such as: 
+// total contribution so far 
+// fines
+// number of months contributed so far
+// Loans taken 
+// Interests paid so far 
+// ... and to do the same for the mt house in general. 
+
