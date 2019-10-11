3650d4bc9c571d9dde6d933e0c07af45f8f584ae
Bosco <nzibosco@gmail.com>
2019-10-11 16:04:59 -0500

    setting up the dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,3 +6,11 @@
 // Interests paid so far 
 // ... and to do the same for the mt house in general. 
 
+import React from "react";
+import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Route, Redirect } from "react-router";
+import { Input, FormBtn, FormRow, TextArea } from "../components/form tools/form";
+import API from "../utils/memberAPI/API"; 
+
