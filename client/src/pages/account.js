8e92fc7f88f9f80746bca5f0fdebcde3c15df562
Bosco <nzibosco@gmail.com>
2019-10-13 14:46:02 -0500

    some styles

--- client/src/pages/account.js
+++ client/src/pages/account.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Input, FormBtn, FormRow, TextArea } from "../components/form tools/form";
 import API from "../utils/memberAPI/API";
 import Jumbotron from "../components/Jumbotron";
-import { Col, Row, container, Container} from "../components/Grid";
+import { Col, Row, container, Container } from "../components/Grid";
 
 class Account extends Component {
     // setting a state representing customer details
@@ -18,7 +18,7 @@ class Account extends Component {
         country: "",
         photo: ""
     };
-        componentDidMount() {
+    componentDidMount() {
         this.loadMembers();
     };
 
@@ -26,7 +26,7 @@ class Account extends Component {
     loadMembers = () => {
         API.getById(2) // to figure out how to insert the user id inside the getById API call
             .then(res => {
-                this.setState({ 
+                this.setState({
                     // members: res.data
                     firstName: res.data.firstName,
                     lastName: res.data.lastName,
@@ -37,9 +37,9 @@ class Account extends Component {
                     state: res.data.state,
                     country: res.data.country,
                     photo: res.data.photo
-                 });
+                });
                 console.log(res.data)
-                })
+            })
             .catch(err => console.log(err));
     };
 
@@ -48,38 +48,43 @@ class Account extends Component {
         return (
             <Container fluid>
                 <Row>
-                    <Col size = "sm-12">
-                        <img src = "http://www.racemph.com/wp-content/uploads/2016/09/profile-image-placeholder.png" style = {{height: "180px", width: "200px", float: "left", clear: "both", marginRight: "20px" }}></img>
+                    <Col size="sm-12">
+                        <img src="http://www.racemph.com/wp-content/uploads/2016/09/profile-image-placeholder.png" style={{ height: "180px", width: "200px", float: "left", clear: "both", marginRight: "20px" }}></img>
                         <br></br>
-                        <div id = "details" style = {{float: "left"}}>
+                        <div id="details" style={{ float: "left" }}>
                             <h5>{this.state.firstName} {this.state.lastName}</h5>
                             <p>Phone: {this.state.phone}</p>
                             <p>Email: {this.state.email}</p>
-                            <p>City: {this.state.city}</p> 
-                            <br/>
+                            <p>City: {this.state.city}</p>
+                            <br />
                             <a class="btn btn-primary" href="#" role="button"><strong>Edit Your Profile</strong></a>
                         </div>
-                        
-                    </Col>
-                    
-                </Row>
-                <Row>
-                    <Col size = "sm-12" id = "options">
-                        {/* include here a warning message that there is contributions or loan repayment due */}
-                        <button type= "submit">Pay contribution</button> 
-                        <br/>
-                        <button type = "submit">Apply for a loan</button>
-                    </Col>
-                </Row>
-                <div className = "summary col-sm-12">
-                    <h3>Your Account Summary</h3>
-                    <p>Total contributions: </p>
-                    <p>Next contribution due date: </p>
-                    <p>Outstanding loan balance: </p>
 
-                </div>
-               <hr/>
-                
+                    </Col>
+
+                </Row>
+
+                <hr />
+                <Row>
+                    <Col size="sm-6" id="options">
+                        {/* include here a warning message that there is contributions or loan repayment due */}
+                        <button type="submit">Pay contribution</button>
+                    </Col>
+                    <Col size = "sm-6">
+                        <button type="submit">Apply for a loan</button>
+                    </Col>
+                </Row>
+                <br/>
+                <Row>
+                    <div className="summary col-sm-12">
+                        <h3>Your Account Summary</h3>
+                        <br/>
+                        <p>Total contributions: </p>
+                        <p>Next contribution due date: </p>
+                        <p>Outstanding loan balance: </p>
+
+                    </div>
+                </Row>
 
             </Container>
 
