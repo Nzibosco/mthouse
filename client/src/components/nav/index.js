4fec1ea173c2d4c9e203739b2aeccca3790cb5e8
Bosco <nzibosco@gmail.com>
2019-10-19 15:24:58 -0500

    more items on the nav list

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -3,7 +3,21 @@ import React from "react";
 function Nav({ children }) {
   return (
 <nav className = "navbar navbar-expand-lg navbar-light bg-light text-center">
-<a class="Display-4 text-center" href="/">MTHouse</a>
+<a class=" nav-brand Display-4 text-center" href="/">MTHouse</a>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="/dashboard">Dashboard</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/loanApplication">Loan Application</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/accounting">Accounting</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/member/3">Profile</a>
+  </li>
+</ul>
 {children}
 </nav>
   );
